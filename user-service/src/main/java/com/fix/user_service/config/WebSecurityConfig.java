5eb5282865676f073e5e8621e461065c57da0ad6
zapzookj <zapzook@naver.com>
2025-04-07 02:41:51 +0900

    fix : 버그 수정

--- user-service/src/main/java/com/fix/user_service/config/WebSecurityConfig.java
+++ user-service/src/main/java/com/fix/user_service/config/WebSecurityConfig.java
@@ -22,7 +22,7 @@ public class WebSecurityConfig {
         return http
             .securityMatcher("/**")
             .authorizeHttpRequests((authorize) -> authorize
-                .requestMatchers("/api/v1/auth/sign-in", "/api/v1/auth/sign-up", "/api/v1/auth/user-info/**").permitAll()
+                .requestMatchers("/api/v1/auth/sign-in", "/api/v1/users/sign-up", "/api/v1/auth/user-info/**").permitAll()
                 .anyRequest().authenticated()
             )
             .csrf((csrf) -> csrf.disable())
