0dab25a01313028426dc6f104c1a78cdd8df4a73
ssu <74248726+azuressu@users.noreply.github.com>
2025-04-07 20:51:39 +0900

    [FP-121] refactor : common 모듈 auditor 수정

---
+++ common-service/src/main/java/com/fix/common_service/config/AuditorAwareImpl.java
@@ -0,0 +1,30 @@
+package com.fix.common_service.config;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component("auditorAware")
+public class AuditorAwareImpl implements AuditorAware<Long> {
+
+	@Override
+	public Optional<Long> getCurrentAuditor() {
+		ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+		if (attributes == null) return Optional.empty();
+
+		HttpServletRequest request = attributes.getRequest();
+		String userIdHeader = request.getHeader("X-User-Id");
+
+		try {
+			return Optional.ofNullable(userIdHeader).map(Long::valueOf);
+		} catch (NumberFormatException e) {
+			return Optional.empty(); // 유효하지 않으면 null
+		}
+	}
+}
