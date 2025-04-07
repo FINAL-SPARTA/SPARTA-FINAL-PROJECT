e27509ff0bd7f8c9d71ba6248bafb2db8f6aa367
ssu <74248726+azuressu@users.noreply.github.com>
2025-04-07 19:52:56 +0900

    [FP-105] feat : Game Read 로직 생성

--- game-service/src/main/java/com/fix/game_service/presentation/controller/GameController.java
+++ game-service/src/main/java/com/fix/game_service/presentation/controller/GameController.java
@@ -2,8 +2,11 @@ package com.fix.game_service.presentation.controller;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +18,10 @@ import com.fix.common_service.dto.CommonResponse;
 import com.fix.common_service.entity.UserRole;
 import com.fix.game_service.application.aop.ValidateUser;
 import com.fix.game_service.application.dtos.request.GameCreateRequest;
+import com.fix.game_service.application.dtos.request.GameSearchRequest;
 import com.fix.game_service.application.dtos.response.GameCreateResponse;
+import com.fix.game_service.application.dtos.response.GameGetOneResponse;
+import com.fix.game_service.application.dtos.response.GameListResponse;
 import com.fix.game_service.application.service.GameService;
 
 import lombok.RequiredArgsConstructor;
@@ -38,6 +44,14 @@ public class GameController {
 		GameCreateResponse response = gameService.createGame(request);
 		return ResponseEntity.ok(CommonResponse.success(
 			response, "경기 생성 성공"
+		));
+	}
+
+	@GetMapping("/{gameId}")
+	public ResponseEntity<CommonResponse<GameGetOneResponse>> getOneGame(@PathVariable UUID gameId) {
+		GameGetOneResponse response = gameService.getOneGame(gameId);
+		return ResponseEntity.ok(CommonResponse.success(
+			response, "경기 단건 조회 성공"
 		));
 	}
 
