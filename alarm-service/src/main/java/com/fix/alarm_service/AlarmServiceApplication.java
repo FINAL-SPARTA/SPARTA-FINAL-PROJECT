e66dcaf887a51c1e649e8d0fd06ce6c726890230
dbp-jack <dbp100402@gmail.com>
2025-04-11 20:10:02 +0900

    [FP-181] 자르 및 도커 이미지 파일 생성

--- alarm-service/src/main/java/com/fix/alarm_service/AlarmServiceApplication.java
+++ alarm-service/src/main/java/com/fix/alarm_service/AlarmServiceApplication.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+
+@SpringBootApplication(scanBasePackages = {"com.fix.common_service", "com.fix.alarm_service"})
+
 public class AlarmServiceApplication {
 
 	public static void main(String[] args) {
