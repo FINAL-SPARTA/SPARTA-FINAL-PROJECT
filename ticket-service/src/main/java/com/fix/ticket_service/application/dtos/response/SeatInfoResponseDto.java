bf0257d0d47c6a6af43b64ed5a122ffbf76bc3c7
zapzookj <zapzook@naver.com>
2025-04-14 11:33:13 +0900

    refactor : Stadium 서버의 구역 별 좌석 조회에 price 포함

--- ticket-service/src/main/java/com/fix/ticket_service/application/dtos/response/SeatInfoResponseDto.java
+++ ticket-service/src/main/java/com/fix/ticket_service/application/dtos/response/SeatInfoResponseDto.java
@@ -14,7 +14,6 @@ public class SeatInfoResponseDto {
     private String section;
     private String seatRow;
     private String seatNumber;
-    // 여기서 가격을 가져오면 티켓 예매 시 가격도 Request 로 넣을 수 있나??
-//    private int price;
+    private int price;
 }
 
