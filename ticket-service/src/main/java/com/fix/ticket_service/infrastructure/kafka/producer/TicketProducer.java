1780097cc1260f8f3f0da0050388444fc3993d04
zapzookj <zapzook@naver.com>
2025-04-14 21:45:59 +0900

    [FP-200] feat : Ticket <-> Order Feign 연동 => Kafka 를 활용한 이벤트 기반 비동기 처리 방식으로 개선

---
+++ ticket-service/src/main/java/com/fix/ticket_service/infrastructure/kafka/producer/TicketProducer.java
@@ -0,0 +1,30 @@
+package com.fix.ticket_service.infrastructure.kafka.producer;
+
+import com.fix.common_service.dto.EventKafkaMessage;
+import com.fix.common_service.dto.TicketReservedPayload;
+import com.fix.ticket_service.domain.model.Ticket;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class TicketProducer {
+
+    private final KafkaTemplate<String, EventKafkaMessage> kafkaTemplate;
+
+    public void sendTicketReservedEvent(List<Ticket> tickets, Long userId) {
+        List<TicketReservedPayload.TicketDetail> ticketDetails = tickets.stream()
+                .map(ticket -> new TicketReservedPayload.TicketDetail(ticket.getTicketId(), ticket.getPrice()))
+                .toList();
+        TicketReservedPayload payload = new TicketReservedPayload(ticketDetails, userId, tickets.get(0).getGameId());
+
+        EventKafkaMessage eventMessage = new EventKafkaMessage("TICKET_RESERVED", payload);
+        kafkaTemplate.send("ticket-reserved-topic", eventMessage);
+        log.info("[Kafka] TICKET_RESERVED 이벤트 발행: {}", eventMessage);
+    }
+}
