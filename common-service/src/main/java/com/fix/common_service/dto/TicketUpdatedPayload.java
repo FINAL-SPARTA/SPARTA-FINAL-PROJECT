3a65185419bcab65e5b9beb5d848639ec4155426
zapzookj <zapzook@naver.com>
2025-04-14 22:00:24 +0900

    [FP-201] feat : Ticket <-> Game Feign 연동 => Kafka 를 활용한 이벤트 기반 비동기 처리 방식으로 개선

---
+++ common-service/src/main/java/com/fix/common_service/dto/TicketUpdatedPayload.java
@@ -0,0 +1,17 @@
+package com.fix.common_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketUpdatedPayload {
+    private UUID gameId;
+    private int quantity;
+}
