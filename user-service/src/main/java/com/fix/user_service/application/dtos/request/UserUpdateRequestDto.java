04567b96511a5a5b60ba9a2c46037670e6ee86af
nimpa3201 <nimpa3201@naver.com>
2025-04-14 21:55:09 +0900

    [FP-203] feat : 카카오 API 연동 위해 user entity에 phoneNumber 필드 추가

--- user-service/src/main/java/com/fix/user_service/application/dtos/request/UserUpdateRequestDto.java
+++ user-service/src/main/java/com/fix/user_service/application/dtos/request/UserUpdateRequestDto.java
@@ -2,6 +2,7 @@ package com.fix.user_service.application.dtos.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -18,4 +19,8 @@ public class UserUpdateRequestDto {
     private String email;
 
     private String roleName; // 선택사항: 역할도 변경 가능하도록
+
+    @NotBlank(message = "전화 번호는 필수 입력값입니다.")
+    @Pattern(regexp = "^010\\d{8}$", message = "전화번호는 010으로 시작하고 숫자 11자리여야 합니다.")
+    private String phoneNumber;
 }
--- user-service/src/main/java/com/fix/user_service/domain/User.java
+++ user-service/src/main/java/com/fix/user_service/domain/User.java
@@ -36,36 +36,41 @@ public class User extends Basic {
     @Column(nullable = false)
     private UserRole roleName;
 
+    private String phoneNumber;
+
     private Integer point;
 
 //    빌더 패턴
     @Builder
-    private User(String username, String email, String password, String nickname, UserRole roleName) {
+    private User(String username, String email, String password, String nickname, UserRole roleName , String phoneNumber) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.nickname = nickname;
         this.roleName = roleName;
+        this.phoneNumber = phoneNumber;
         this.point = 0;
     }
 
 
 //    정적팩토리 메서드
-    public static User create(String username, String email, String encodedPassword, String nickname, UserRole roleName) {
+    public static User create(String username, String email, String encodedPassword, String nickname, UserRole roleName, String phoneNumber) {
         return User.builder()
             .username(username)
             .email(email)
             .password(encodedPassword)
             .nickname(nickname)
             .roleName(roleName)
+            .phoneNumber(phoneNumber)
             .build();
     }
 
 //    수정 메서드
-    public void update(String nickname, String email, UserRole roleName) {
+    public void update(String nickname, String email, UserRole roleName, String phoneNumber) {
         this.nickname = nickname;
         this.email = email;
         this.roleName = roleName;
+        this.phoneNumber = phoneNumber;
     }
 
     public void deductPoints(Integer requiredPoints) {
