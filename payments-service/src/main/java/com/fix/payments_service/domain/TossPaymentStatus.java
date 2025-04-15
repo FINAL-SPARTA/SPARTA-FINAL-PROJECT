88d72f6913739051bdb6d7995c61651dab034273
dbp-jack <dbp100402@gmail.com>
2025-04-15 11:02:35 +0900

    [FP-213] feat/payments - TossPayment 결제 내역 저장

---
+++ payments-service/src/main/java/com/fix/payments_service/domain/TossPaymentStatus.java
@@ -0,0 +1,12 @@
+package com.fix.payments_service.domain;
+
+public enum TossPaymentStatus {
+    READY,
+    IN_PROGRESS,
+    WAITING_FOR_DEPOSIT,
+    DONE,
+    CANCELED,
+    PARTIAL_CANCELED,
+    ABORTED,
+    EXPIRED
+}
