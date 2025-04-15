c4e11df89c1441914e42e8c1e4816094ba9ec9cb
dbp-jack <dbp100402@gmail.com>
2025-04-15 09:46:29 +0900

    [FP-211] feat/payments - feign 추가

---
+++ payments-service/src/main/java/com/fix/payments_service/infrastructure/client/OrderServiceClient.java
@@ -0,0 +1,12 @@
+package com.fix.payments_service.infrastructure.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "order-service", url = "${feign.order-service.url}")
+public interface OrderServiceClient {
+
+    @PatchMapping("/api/v1/orders/{orderId}/complete")
+    void completeOrder(@PathVariable("orderId") String orderId);
+}
