bfdd5c2f9297ea496fdc08197ffd8202030b451b
ssu <74248726+azuressu@users.noreply.github.com>
2025-04-15 09:45:35 +0900

    [FP-197] refactor : 채팅방 단건 조회에 조건 추가

--- chat-service/src/main/java/com/fix/chat_service/application/dtos/response/ChatRoomGetOneResponse.java
+++ chat-service/src/main/java/com/fix/chat_service/application/dtos/response/ChatRoomGetOneResponse.java
@@ -17,12 +17,14 @@ public class ChatRoomGetOneResponse {
 
 	private UUID chatId;
 	private String gameName;
+	private String message;
 
 	public static ChatRoomGetOneResponse fromChatRoom(ChatRoom chatRoom) {
 		return ChatRoomGetOneResponse
 			.builder()
 			.chatId(chatRoom.getChatRoomId())
 			.gameName(chatRoom.getGameName())
+			.message("채팅방 입장 성공")
 			.build();
 	}
 }
--- chat-service/src/main/java/com/fix/chat_service/presenatation/controller/ChatController.java
+++ chat-service/src/main/java/com/fix/chat_service/presenatation/controller/ChatController.java
@@ -1,13 +1,22 @@
 package com.fix.chat_service.presenatation.controller;
 
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
 import com.fix.chat_service.application.dtos.request.ChatCreateRequest;
+import com.fix.chat_service.application.dtos.response.ChatRoomGetOneResponse;
 import com.fix.chat_service.application.service.ChatService;
 
+import jakarta.ws.rs.Path;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,6 +32,12 @@ public class ChatController {
 	void createChatRoom(@RequestBody ChatCreateRequest request) {
 		log.info(request.getGameName());
 		chatService.createChatRoom(request);
+	}
+
+	@GetMapping("/{chatId}/info")
+	public ResponseEntity<ChatRoomGetOneResponse> getGameInfo(@PathVariable UUID chatId) {
+		ResponseEntity<ChatRoomGetOneResponse> response = chatService.getChatRoom(chatId);
+		return response;
 	}
 
 }
