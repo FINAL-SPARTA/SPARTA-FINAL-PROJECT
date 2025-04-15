15fbe7308e970650982aa21b82053bb13f2788aa
dbp-jack <dbp100402@gmail.com>
2025-04-15 11:02:35 +0900

    [FP-213] feat/payments - TossPayment 결제 내역 저장

---
+++ payments-service/src/main/java/com/fix/payments_service/domain/TossPaymentMethod.java
@@ -0,0 +1,13 @@
+package com.fix.payments_service.domain;
+
+public enum TossPaymentMethod {
+    CARD,
+    VIRTUAL_ACCOUNT,
+    ACCOUNT_TRANSFER,
+    MOBILE_PHONE,
+    TOSS_PAY,
+    PAYCO,
+    SIMPLE_PAYMENT,
+    GIFT_CARD,
+    CULTURE_GIFT_CARD
+}
