4975a562c1c9dd1ffd2c088695c570e19169089c
ssu <74248726+azuressu@users.noreply.github.com>
2025-04-15 16:21:53 +0900

    [FP-216] refactor : 티켓 - 경기 수신 메시지 타입 수정

--- game-service/src/main/java/com/fix/game_service/presentation/controller/ConsumerController.java
+++ game-service/src/main/java/com/fix/game_service/presentation/controller/ConsumerController.java
@@ -1,12 +1,10 @@
 package com.fix.game_service.presentation.controller;
 
-import org.springframework.kafka.annotation.KafkaListener;
-
-import com.fix.game_service.application.dtos.request.GameTicketRequest;
+import com.fix.common_service.dto.EventKafkaMessage;
 import com.fix.game_service.application.service.GameService;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,13 +12,16 @@ public class ConsumerController {
 
 	private final GameService gameService;
 
-	// Kafka에서 메시지를 소비하는 리스너 메서드들을 정의
-	// @KafkaListener 어노테이션은 각 메서드를 Kafka 리스너로 설정
-	@KafkaListener(topics = "game-events", groupId = "group_a")
+	/**
+	 * Kafka에서 메시지 소비를 위한 리스너 메서드 정의
+	 * @param data : 주고 받을 대화
+	 */
+	@KafkaListener(topics = "ticket-updated-topic", groupId = "game-service-group")
+	// 각 메서드들을 Kafka 리스너로 설정
 	public void consume(Object data) {
-		if (data instanceof GameTicketRequest dto) {
-			log.info("Received DTO: {}", dto);
-			gameService.updateGameSeats(dto.getGameId(), dto.getQuantity());
+		if (data instanceof EventKafkaMessage message) {
+			log.info("Received DTO: {}", message);
+			gameService.updateGameSeatsByConsumer(message);
 		} else {
 			log.warn("Unknown message type: {}", data);
 		}
