1dc41a31e2daad24d51537160c69c8610e8f9218
ssu <74248726+azuressu@users.noreply.github.com>
2025-04-15 09:45:35 +0900

    [FP-197] refactor : 채팅방 단건 조회에 조건 추가

--- chat-service/src/main/java/com/fix/chat_service/application/service/ChatService.java
+++ chat-service/src/main/java/com/fix/chat_service/application/service/ChatService.java
@@ -1,7 +1,9 @@
 package com.fix.chat_service.application.service;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.fix.chat_service.application.dtos.request.ChatCreateRequest;
@@ -40,9 +42,24 @@ public class ChatService {
 	 * @param chatId : 채팅방 ID
 	 * @return : 채팅방 정보 반환
 	 */
-	public ChatRoomGetOneResponse getChatRoom(UUID chatId) {
+	public ResponseEntity<ChatRoomGetOneResponse> getChatRoom(UUID chatId) {
 		ChatRoom chatRoom = findChatRoom(chatId);
-		return ChatRoomGetOneResponse.fromChatRoom(chatRoom);
+		// 시간 조건 체크
+		if (LocalDateTime.now().isBefore(chatRoom.getChatOpenDate())) {
+			return ResponseEntity
+				.badRequest()
+				.body(ChatRoomGetOneResponse.builder().message("아직 채팅방 오픈 전입니다.").build());
+		}
+
+		if (LocalDateTime.now().isAfter(chatRoom.getChatCloseDate())) {
+			return ResponseEntity
+				.badRequest()
+				.body(ChatRoomGetOneResponse.builder().message("채팅방 이용 시간이 종료되었습니다.").build());
+		}
+
+		ChatRoomGetOneResponse response = ChatRoomGetOneResponse.fromChatRoom(chatRoom);
+		return ResponseEntity
+			.ok().body(response);
 	}
 
 	/**
