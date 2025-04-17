7b5a7a51b77c8d10ff3cc379c5ee4ec2544e1bc2
ssu <74248726+azuressu@users.noreply.github.com>
2025-04-17 21:47:29 +0900

    [FP-228] refactor : Lock 쿼리문 추가

--- game-service/src/main/java/com/fix/game_service/domain/repository/GameRepository.java
+++ game-service/src/main/java/com/fix/game_service/domain/repository/GameRepository.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 import com.fix.game_service.domain.model.Game;
 
@@ -11,5 +12,6 @@ import jakarta.persistence.LockModeType;
 
 public interface GameRepository extends JpaRepository<Game, UUID>, GameRepositoryCustom {
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("select g from Game g where g.gameId = :gameId")
 	Game findByIdWithLock(UUID gameId);
 }
