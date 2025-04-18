79e42459a3941e3d0ca1539648165e06728e3641
zapzookj <zapzook@naver.com>
2025-04-18 11:42:48 +0900

    feat : 카프카 Topic 설정, TicketProducer 수정

--- ticket-service/src/main/java/com/fix/ticket_service/TicketServiceApplication.java
+++ ticket-service/src/main/java/com/fix/ticket_service/TicketServiceApplication.java
@@ -3,9 +3,11 @@ package com.fix.ticket_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan(basePackages = {"com.fix.common_service", "com.fix.ticket_service"})
 public class TicketServiceApplication {
 
 	public static void main(String[] args) {
