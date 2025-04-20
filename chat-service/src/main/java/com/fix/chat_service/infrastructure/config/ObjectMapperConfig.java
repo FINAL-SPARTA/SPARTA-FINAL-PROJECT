3fc6073e5d82584f0dd2d4e97e2930c3e632a86e
ssu <74248726+azuressu@users.noreply.github.com>
2025-04-20 16:45:55 +0900

    [FP-246] refactor : ObjectMapper에 사용할 의존성 추가

--- chat-service/src/main/java/com/fix/chat_service/infrastructure/config/ObjectMapperConfig.java
+++ chat-service/src/main/java/com/fix/chat_service/infrastructure/config/ObjectMapperConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 
 @Configuration
 public class ObjectMapperConfig {
@@ -13,7 +14,8 @@ public class ObjectMapperConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule()); // JavaTimeModule 등록
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new AfterburnerModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
