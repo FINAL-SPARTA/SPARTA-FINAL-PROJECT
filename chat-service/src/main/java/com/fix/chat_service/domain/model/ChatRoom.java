911ae5b656d1587f53d141999ce961bb28f509b3
ssu <74248726+azuressu@users.noreply.github.com>
2025-04-22 22:24:19 +0900

    [FP-263] feat : 채팅방 전체조회, 수정, 상태 수정, 삭제 기능 추가

--- chat-service/src/main/java/com/fix/chat_service/domain/model/ChatRoom.java
+++ chat-service/src/main/java/com/fix/chat_service/domain/model/ChatRoom.java
@@ -1,6 +1,7 @@
 package com.fix.chat_service.domain.model;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -12,14 +13,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
 @Getter
 @Builder
 @Entity
 @Table(name = "p_chatroom")
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChatRoom {
+public class ChatRoom extends Basic {
 
 	@Id
 	@Column(name = "chat_room_id")
@@ -42,6 +42,27 @@ public class ChatRoom {
 
 	@Column(name = "game_status")
 	private String gameStatus;
+
+	/**
+	 * 채팅방 내용 수정
+	 * @param updateChatRoom : 수정할 채팅방 내용
+	 */
+	public void updateChatRoomInfo(ChatRoom updateChatRoom) {
+		Optional.ofNullable(updateChatRoom.getGameName()).ifPresent(gameName -> this.gameName = gameName);
+		Optional.ofNullable(updateChatRoom.getGameDate()).ifPresent(gameDate -> this.gameDate = gameDate);
+		Optional.ofNullable(updateChatRoom.getChatOpenDate())
+			.ifPresent(chatOpenDate -> this.chatOpenDate = chatOpenDate);
+		Optional.ofNullable(updateChatRoom.getChatCloseDate())
+			.ifPresent(chatCloseDate -> this.chatCloseDate = chatCloseDate);
+	}
+
+	/**
+	 * 채팅방 상태(경기 상태) 수정
+	 * @param updateChatRoomStatus : 수정할 채팅방(경기) 상태
+	 */
+	public void updateChatRoomStatus(ChatRoom updateChatRoomStatus) {
+		Optional.ofNullable(updateChatRoomStatus.getGameStatus()).ifPresent(gameStatus -> this.gameStatus = gameStatus);
+	}
 
 }
 
