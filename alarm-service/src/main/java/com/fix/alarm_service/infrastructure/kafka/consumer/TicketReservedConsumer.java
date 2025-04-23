9d44b690e5cd1e3ee4238e15edd3b94dd786a073
nimpa3201 <nimpa3201@naver.com>
2025-04-23 15:29:16 +0900

    [FP-268] feat : OrderCompletedConsumer 구현

--- alarm-service/src/main/java/com/fix/alarm_service/infrastructure/kafka/consumer/TicketReservedConsumer.java
+++ alarm-service/src/main/java/com/fix/alarm_service/infrastructure/kafka/consumer/TicketReservedConsumer.java
@@ -6,7 +6,7 @@ import com.fix.alarm_service.application.service.AlarmService;
 import com.fix.common_service.kafka.consumer.AbstractKafkaConsumer;
 import com.fix.common_service.kafka.consumer.RedisIdempotencyChecker;
 import com.fix.common_service.kafka.dto.EventKafkaMessage;
-import com.fix.common_service.kafka.dto.TicketReservedPayload;
+import com.fix.common_service.kafka.dto.OrderCompletedPayload;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,31 +14,31 @@ import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 @Slf4j
 @Component
-public class TicketReservedConsumer extends AbstractKafkaConsumer<TicketReservedPayload> {
+public class TicketReservedConsumer extends AbstractKafkaConsumer<OrderCompletedPayload> {
     private final AlarmService alarmService;
     public TicketReservedConsumer(RedisIdempotencyChecker idempotencyChecker, AlarmService alarmService) {
         super(idempotencyChecker);
         this.alarmService = alarmService;
     }
     @KafkaListener(
-            topics = "${kafka-topics.ticket.reserved}",
+            topics = "${kafka-topics.order.completed}",
             groupId = "alarm-service-ticket-reserved-consumer"
     )
-    public void listen(ConsumerRecord<String, EventKafkaMessage<TicketReservedPayload>> record,
-                       EventKafkaMessage<TicketReservedPayload> message,
+    public void listen(ConsumerRecord<String, EventKafkaMessage<OrderCompletedPayload>> record,
+                       EventKafkaMessage<OrderCompletedPayload> message,
                        Acknowledgment ack) {
         super.consume(record, message, ack);
     }
     @Override
     protected void processPayload(Object rawPayload) {
-        TicketReservedPayload payload = mapPayload(rawPayload, TicketReservedPayload.class);
+        OrderCompletedPayload payload = mapPayload(rawPayload,OrderCompletedPayload.class);
         try {
             // 유저 전화번호 조회
             PhoneNumberResponseDto phone = alarmService.getPhoneNumber(payload.getUserId());
             // 알림 메시지 생성
             String message = String.format(
                     "[예매 완료]  티켓이 성공적으로 예매되었습니다. 총 %d매.",
-                    payload.getTicketDetails().size()
+                    payload.getTicketIds().size()
             );
             alarmService.sendSns(phone.getPhoneNumber(), message);
             log.info("[Kafka][Alarm] 유저 {} 에게 예매 완료 알림 전송 성공", payload.getUserId());
