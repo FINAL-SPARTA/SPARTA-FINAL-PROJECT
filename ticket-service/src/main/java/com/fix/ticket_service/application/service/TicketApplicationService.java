2eb620651951f67ba5b60703b5f6c659ed09daca
zapzookj <zapzook@naver.com>
2025-04-24 17:24:31 +0900

    refactor : Ticket <-> Game 이벤트 토픽 두 개로 분리 (티켓 판매, 티켓 취소)

--- ticket-service/src/main/java/com/fix/ticket_service/application/service/TicketApplicationService.java
+++ ticket-service/src/main/java/com/fix/ticket_service/application/service/TicketApplicationService.java
@@ -8,8 +8,6 @@ import com.fix.ticket_service.application.exception.TicketException;
 import com.fix.ticket_service.domain.model.Ticket;
 import com.fix.ticket_service.domain.model.TicketStatus;
 import com.fix.ticket_service.domain.repository.TicketRepository;
-import com.fix.ticket_service.infrastructure.client.GameClient;
-import com.fix.ticket_service.infrastructure.client.OrderClient;
 import com.fix.ticket_service.infrastructure.client.StadiumClient;
 import com.fix.ticket_service.infrastructure.kafka.producer.TicketProducer;
 import lombok.RequiredArgsConstructor;
@@ -36,8 +34,6 @@ import java.util.stream.Collectors;
 public class TicketApplicationService {
 
     private final TicketRepository ticketRepository;
-    private final OrderClient orderClient;
-    private final GameClient gameClient;
     private final StadiumClient stadiumClient;
     private final RedissonClient redissonClient;
     private final TicketProducer ticketProducer;
@@ -216,7 +212,7 @@ public class TicketApplicationService {
 
         // 3) 티켓 업데이트 이벤트 발행 (경기 서버에 잔여 좌석 차감 요청)
         int quantity = tickets.size();
-        ticketProducer.sendTicketUpdatedEvent(tickets.get(0).getGameId(), -quantity);
+        ticketProducer.sendTicketSoldEvent(tickets.get(0).getGameId(), -quantity);
 
         // 4) Redis 캐시에 SOLD 상태 저장 (TTL = 24시간)
         for (Ticket ticket : tickets) {
@@ -242,7 +238,7 @@ public class TicketApplicationService {
 
         // 3) 티켓 업데이트 이벤트 발행 (경기 서버에 잔여 좌석 증가 요청)
         int quantity = tickets.size();
-        ticketProducer.sendTicketUpdatedEvent(tickets.get(0).getGameId(), quantity);
+        ticketProducer.sendTicketCancelledEvent(tickets.get(0).getGameId(), quantity);
 
         // 4) Redis 캐시에서 해당 좌석 키 삭제
         for (Ticket ticket : tickets) {
