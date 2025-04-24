210eb55ad23bf68490f68e7cd796a2fc87ad39bc
zapzookj <zapzook@naver.com>
2025-04-24 12:23:47 +0900

    feat : order 도메인 - logback 및 p6spy 설정, 예외 핸들러 수정

--- order-service/src/main/java/com/fix/order_service/application/exception/OrderExceptionHandler.java
+++ order-service/src/main/java/com/fix/order_service/application/exception/OrderExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.fix.order_service.application.exception;
 
 import com.fix.common_service.dto.CommonResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,13 +12,42 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class OrderExceptionHandler {
 
     @ExceptionHandler(OrderException.class)
-    public ResponseEntity<CommonResponse<Void>> handleOrderException(OrderException exception) {
-        log.error("[OrderError] code = {}, message = {}", exception.getErrorCode(), exception.getMessage());
+    public ResponseEntity<CommonResponse<Void>> handleOrderException(
+        OrderException exception, HttpServletRequest request) {
+        log.warn("OrderException 발생 : URI={}, Method={}, ErrorCode={}, ErrorMessage={}",
+            request.getRequestURI(), request.getMethod(),
+            exception.getErrorCode(), exception.getMessage());
 
         return ResponseEntity.status(exception.getStatus()).body(CommonResponse.fail(
                 exception.getErrorCode(),
                 exception.getMessage(),
                 exception.getStatus().value()
+        ));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<CommonResponse<Void>> handleIllegalArgumentException(
+        IllegalArgumentException exception, HttpServletRequest request) {
+        log.warn("IllegalArgumentException 발생 : URI={}, Method={}, ErrorMessage={}",
+            request.getRequestURI(), request.getMethod(), exception.getMessage());
+
+        return ResponseEntity.badRequest().body(CommonResponse.fail(
+                "Illegal Argument",
+                exception.getMessage(),
+                400
+        ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommonResponse<Void>> handleException(
+        Exception exception, HttpServletRequest request) {
+        log.error("예상하지 못한 Exception 발생 : URI={}, Method={}, ErrorMessage={}",
+            request.getRequestURI(), request.getMethod(), exception.getMessage());
+
+        return ResponseEntity.internalServerError().body(CommonResponse.fail(
+                "Internal Server Error",
+                exception.getMessage(),
+                500
         ));
     }
 }
