d80cd55d13053c6bffd04a49cd65a17b4e19dc34
ssu <74248726+azuressu@users.noreply.github.com>
2025-04-27 15:31:02 +0900

    [FP-288] feat : GameEvent 전송 스케줄러 추가

--- game-service/src/main/java/com/fix/game_service/domain/repository/GameEventRepository.java
+++ game-service/src/main/java/com/fix/game_service/domain/repository/GameEventRepository.java
@@ -3,5 +3,8 @@ package com.fix.game_service.domain.repository;
 import com.fix.game_service.domain.model.GameEvent;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface GameEventRepository extends MongoRepository<GameEvent, String> {
+    List<GameEvent> findByStatus(String pending);
 }
---
+++ game-service/src/main/java/com/fix/game_service/presentation/scheduler/GameEventScheduler.java
@@ -0,0 +1,55 @@
+package com.fix.game_service.presentation.scheduler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fix.common_service.kafka.dto.GameCreatedInfoPayload;
+import com.fix.game_service.application.exception.GameException;
+import com.fix.game_service.domain.model.GameEvent;
+import com.fix.game_service.domain.repository.GameEventRepository;
+import com.fix.game_service.presentation.controller.GameProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class GameEventScheduler {
+
+    private final GameEventRepository gameEventRepository;
+    private final GameProducer gameProducer;
+    private final ObjectMapper objectMapper;
+
+    @Scheduled(fixedDelay = 3000)
+    public void processGameEventOutbox() {
+        List<GameEvent> pendingGameEvents = gameEventRepository.findByStatus("PENDING");
+
+        for (GameEvent event : pendingGameEvents) {
+            try {
+                GameCreatedInfoPayload payload = convertPayload(event.getEventType(), event.getPayload());
+                gameProducer.sendGameInfoToAlarm(payload);
+
+                // 성공하면 상태 변경
+                event.markAsCompleted();
+                gameEventRepository.save(event);
+
+                log.info("Outbox 이벤트 발송 완료: {}", event.getId());
+            } catch (Exception e) {
+                log.error("Outbox 이벤트 발송 실패: {}", event.getId(), e);
+                throw new GameException(GameException.GameErrorType.GAME_PARSING_ERROR);
+            }
+        }
+    }
+
+    private GameCreatedInfoPayload convertPayload(String eventType, String payload) throws Exception {
+        switch (eventType) {
+            case "GAME_CREATED":
+                return objectMapper.readValue(payload, GameCreatedInfoPayload.class);
+            default:
+                throw new IllegalArgumentException("Unknown eventType: " + eventType);
+        }
+    }
+
+}
