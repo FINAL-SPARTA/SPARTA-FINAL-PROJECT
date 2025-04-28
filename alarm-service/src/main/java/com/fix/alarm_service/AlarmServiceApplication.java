a511c0a3c5f221304dcec45360da174441962121
nimpa3201 <nimpa3201@naver.com>
2025-04-28 19:03:09 +0900

    [FP-294] feat : alarm - order 서비스에서 alarm 서비스 구독 및 해당 게임에 해당하는 userId  발행

--- alarm-service/src/main/java/com/fix/alarm_service/AlarmServiceApplication.java
+++ alarm-service/src/main/java/com/fix/alarm_service/AlarmServiceApplication.java
@@ -3,9 +3,10 @@ package com.fix.alarm_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableKafka
 @EnableFeignClients
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = {"com.fix.common_service", "com.fix.alarm_service"})
--- order-service/src/main/java/com/fix/order_service/domain/repository/OrderRepository.java
+++ order-service/src/main/java/com/fix/order_service/domain/repository/OrderRepository.java
@@ -2,8 +2,14 @@ package com.fix.order_service.domain.repository;
 
 import com.fix.order_service.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+
+    @Query("SELECT DISTINCT o.userId FROM Order o WHERE o.gameId = :gameId")
+    List<Long> findUserIdsByGameId(@Param("gameId") UUID gameId);
 }
