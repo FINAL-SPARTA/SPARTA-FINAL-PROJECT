95f2e84393ec980ec2088185687658a797505295
ssu <74248726+azuressu@users.noreply.github.com>
2025-05-06 21:46:42 +0900

    [FP-299] : 대기열에 Kafka 적용

--- game-service/src/main/java/com/fix/game_service/infrastructure/config/RedisConfig.java
+++ game-service/src/main/java/com/fix/game_service/infrastructure/config/RedisConfig.java
@@ -1,46 +1,75 @@
 package com.fix.game_service.infrastructure.config;
 
-import java.time.Duration;
-
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
-	@Bean
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(factory);
-		template.setKeySerializer(new StringRedisSerializer());
-		return template;
-	}
+    @Value("${spring.data.redis.host}")
+    private String host;
 
-	@Bean
-	public CacheManager cacheManager(RedisConnectionFactory connectionFactory){
-		// Cache에 저장된 값 직렬화 및 TTL 설정
-		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-			.serializeValuesWith(
-				RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
-			)
-			.entryTtl(Duration.ofMinutes(120))
-			.disableCachingNullValues();
+    @Value("${spring.data.redis.port}")
+    private int port;
 
-		// 설정한 CacheConfig 기반으로 RedisCacheManager 생성
-		return RedisCacheManager.builder(connectionFactory)
-			.cacheDefaults(redisCacheConfiguration)
-			.build();
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
 
-	}
+        // 커넥션 풀 설정
+        GenericObjectPoolConfig<Object> poolConfig = new GenericObjectPoolConfig<>();
+        poolConfig.setMaxTotal(100);      // 동시에 열 수 있는 최대 커넥션 수
+        poolConfig.setMaxIdle(50);        // 유휴 상태로 유지할 최대 커넥션
+        poolConfig.setMinIdle(10);        // 유휴 상태로 유지할 최소 커넥션
+        poolConfig.setTestOnBorrow(true); // 커넥션 꺼짐 여부 확인
+        poolConfig.setTestWhileIdle(true);// idle 중 커넥션 유효성 주기적 체크
+
+        LettucePoolingClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(2)) // 응답 타임아웃
+                .poolConfig(poolConfig)
+                .build();
+
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        // Cache에 저장된 값 직렬화 및 TTL 설정
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
+                )
+                .entryTtl(Duration.ofMinutes(120))
+                .disableCachingNullValues();
+
+        // 설정한 CacheConfig 기반으로 RedisCacheManager 생성
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+
+    }
 
 }
